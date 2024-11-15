126665dbcdbdecbd4d912c9d8bce6b38389f8e79
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-11-15 17:54:25 +0300

    Viewsimport

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,7 @@ from .models import Book
 from .models import Library
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
