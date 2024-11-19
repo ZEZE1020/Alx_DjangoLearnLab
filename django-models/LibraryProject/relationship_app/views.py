ae550a7eb9dea59395ebaef7dc4014162fd0dace
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-11-19 19:06:06 +0300

    Add Register.html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -18,6 +18,34 @@ class LibraryDetailView(DetailView):
   template_name = 'relationship_app/library_detail.html'
   context_object_name = 'library'
 
+def register(request): 
+   if request.method == "POST": 
+      form = UserCreationForm(request.POST) 
+   if form.is_valid(): 
+      form.save() 
+      username = form.cleaned_data.get('username') 
+      password = form.cleaned_data.get('password1') 
+      user = authenticate(username=username, password=password) 
+      login(request, user) 
+      return redirect('home') 
+   else: 
+      form = UserCreationForm() 
+   return render(request, 'register.html', {'form': form})
+
+def login_view(request): 
+   if request.method == "POST":
+       form = AuthenticationForm(data=request.POST) 
+   if form.is_valid(): 
+      user = form.get_user() 
+      login(request, user) 
+      return redirect('home') 
+   else: 
+      form = AuthenticationForm() 
+   return render(request, 'login.html', {'form': form})
+
+def logout_view(request): 
+   logout(request) 
+   return render(request, 'logout.html')
 class SignUpView(generic.CreateView):
    form_class = UserCreationForm
    success_url = reverse_lazy('login')
