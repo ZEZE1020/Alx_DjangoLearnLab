f27a843eb49c4431df0f6d938ce3c7abf59e44d5
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-11-20 10:28:15 +0300

    HTMlviews

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 # relationship_app/urls.py
 
 from django.urls import path
+
+path('register/', register, name='register'),
 from .views import list_books, LibraryDetailView
 from django.contrib.auth import views as auth_views      
 from .views import SignUpView, register, login_view, login, logout_view
@@ -12,5 +14,5 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'), 
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    
 ]
