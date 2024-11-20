35e587e2110b672b3c48e10e76b8c6e35de8f875
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-11-20 11:02:29 +0300

    Fix register.html missing

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,14 +2,19 @@
 
 from django.urls import path
 
-path('register/', register, name='register'),
+
 from .views import list_books, LibraryDetailView
 from django.contrib.auth import views as auth_views      
-from .views import SignUpView, register, login_view, login, logout_view
+from .views import SignUpView
+from .views import register
+from .views import login_view
+from .views import login
+from .views import logout_view
 
 
 
 urlpatterns = [
+    path('register/', register, name='register'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'), 
