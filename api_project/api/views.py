c2d6a5f3077848ac5243ead16fe7badd08ea04f5
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-12-08 23:41:27 +0300

    Create view set, Set up router, test CRUD operations in bash

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from rest_framework import generics.ListAPIView
+from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
-
+from rest_framework import viewsets
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+class BookViewSet(viewsets.ModelViewSet): 
+    queryset = Book.objects.all() 
+    serializer_class = BookSerializer
