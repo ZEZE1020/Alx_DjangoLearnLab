8407741233c128a05d23cd161ce75cf415af99c2
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-12-15 23:46:32 +0300

    Setting up generic views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+#List_view for retrieving all books 
+class BookListView(generics.ListCreateAPIView):
+  """ 
+  get: Return a list of all existing books. 
+  post: Create a new book instance.
+  """
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+  """
+   get: Return details of a specific book by ID.
+   put: Update an existing book instance. 
+   delete: Delete a book instance. 
+  """
+  queryset = Book.objects.all()
+  serializer_class = BookSerializer
+  permission_classes = [IsAuthrnticatedOrReadOnly]
 
-# Create your views here.
+class BookCreateView(generics.CreateAPIView):
+	"""
+	post: Create a new book instance. 
+	"""
+queryset = Book.objects.all()
+serializer_class = BookSerializer
+permission_classes = [IsAuthenticatedOrReadOnly]
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
