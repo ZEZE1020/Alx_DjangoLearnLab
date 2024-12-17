299814c739b6a4112f7ec877c73266421cbd0847
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-12-18 00:07:42 +0300

    Fix filtering import for views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,8 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework.filters import SearchFilter
-from rest_framework.filters import OrderingFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters import rest_framework 
 #List_view for retrieving all books 
 class BookListView(generics.ListCreateAPIView):
