9c1111b5f691f00b6881f49ac2113240b21820ab
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-12-18 00:02:27 +0300

    Booklist ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,15 +3,25 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 #List_view for retrieving all books 
 class BookListView(generics.ListCreateAPIView):
   """ 
-  get: Return a list of all existing books. 
-  post: Create a new book instance.
+  get: 
+  Return a list of all existing books. 
+  post: 
+  Create a new book instance.
   """
   queryset = Book.objects.all()
   serializer_class = BookSerializer
   permission_classes = [IsAuthenticatedOrReadOnly]
+  filter_backends = [DjangoFilterBackend, SearchFilter]
+  filterset_class = BookFilter
+  search_fields = ['title', 'author__name'] 
+  ordering_fields = ['title', 'publication_year']
+  
+  
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
   """
    get: Return details of a specific book by ID.
