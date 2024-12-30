8c0b41bd4b7ddcc353a685550c4bfa2c64e7d053
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-12-30 17:49:41 +0300

    Add  Searchfilters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -17,7 +17,7 @@ class BookListView(generics.ListCreateAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer
   permission_classes = [IsAuthenticatedOrReadOnly]
-  filter_backends = [filters.OrderingFilter]
+  filter_backends = [filters.OrderingFilter, filters.SearchFilter]
   filterset_class = BookFilter
   filterset_fields = ['title','author', 'publication_year']
   search_fields = ['title', 'author'] 
