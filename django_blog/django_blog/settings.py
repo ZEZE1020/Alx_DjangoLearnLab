1b72bfd07c8e0c4ac5b0b69ae490f6ee3496f5ae
ZEZE1020 <ogembogodfrey75@gmail.com>
2025-01-09 02:13:43 +0300

    database set up

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,13 +74,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+         'ENGINE': 'django.db.backends.postgresql', 
+         'NAME': 'blog', 
+         'USER': 'ogembo', 
+         'PASSWORD': '', 
+         'HOST': 'localhost', 
+         'PORT': '5432', } }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
