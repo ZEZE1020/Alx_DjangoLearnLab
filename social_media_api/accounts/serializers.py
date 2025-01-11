cf09ce1df0562a593ce57a1f3a262ec8fdf60c67
ZEZE1020 <ogembogodfrey75@gmail.com>
2025-01-11 22:10:17 +0300

    token serializer, user create update

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -6,6 +6,7 @@ User = get_user_model()
 
 class CustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    serializers.CharField()
     class Meta:
         model = User
         fields = ('id', 'username', 'password', 'email')
